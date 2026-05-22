5a65c5020273485f95e715f492be11999ce4ee4b
msceada1 <195111856+msceada1@users.noreply.github.com>
2026-05-22 08:57:39 +0200

    terminando tablero

--- proyecto/code/main.py
+++ proyecto/code/main.py
@@ -8,6 +8,16 @@ class Main:
 
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('Snake')
+
+        # game objects
+        self.bg_rects = [pygame.Rect((col + int(row % 2 == 0)) * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+                         for col in range(0, COLS, 2) for row in range(ROWS)]
+
+    # dando color a la celda
+    def draw_bg(self):
+        for rect in self.bg_rects:
+            pygame.draw.rect(self.display_surface, DARK_GREEN, rect)
 
     def run(self):
         while True:
@@ -15,9 +25,11 @@ class Main:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     exit()
-
+            self.display_surface.fill(LIGHT_GREEN)  # color de fondo en verde
+            self.draw_bg()
             pygame.display.update()
 
 
-main = Main()
-main.run()
+if __name__ == '__main__':
+    main = Main()
+    main.run()
